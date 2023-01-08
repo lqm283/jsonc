cf3efacfbba462fea12d32bddbe38470828556aa
lqm283 <lanqianming@hotmail.com>
2023-01-08 19:22:23 +0800

    完成 多元素 Bool 类型的 json 转换为保存 Num 类型的单成员 char 的测试

--- test/include/json_to_mult.h
+++ test/include/json_to_mult.h
@@ -1,7 +1,7 @@
 /*
  * @Author       : lqm283
  * @Date         : 2023-01-06 12:58:17
- * @LastEditTime : 2023-01-08 18:58:13
+ * @LastEditTime : 2023-01-08 19:11:54
  * @LastEditors  : lqm283
  * --------------------------------------------------------------------------------<
  * @Description  : Please edit a descrition about this file at here.
@@ -28,6 +28,8 @@ static const struct TestChangeToMult ChangeToMult[] = {
     FUNC(test_change_mult_str_json_to_non_non_single_char_num),
     FUNC(test_change_single_num_json_to_non_non_single_char_num),
     FUNC(test_change_mult_num_json_to_non_non_single_char_num),
+    FUNC(test_change_single_bool_json_to_non_non_single_char_num),
+    FUNC(test_change_mult_bool_json_to_non_non_single_char_num),
     {NULL, NULL}};
 
 #endif
