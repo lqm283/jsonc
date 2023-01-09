1e240c9c0c7e1e4aeae5c3b60dfbdbcae652fb81
lqm283 <lanqianming@hotmail.com>
2023-01-09 10:50:25 +0800

    完成 完全匹配的多元素 Bool 类型的 json 转换为保存 Num 类型的多成员 char 的测试

--- test/include/test_non_non_char.h
+++ test/include/test_non_non_char.h
@@ -1,7 +1,7 @@
 /*
  * @Author       : lqm283
  * @Date         : 2023-01-06 09:07:42
- * @LastEditTime : 2023-01-09 09:34:35
+ * @LastEditTime : 2023-01-09 10:39:17
  * @LastEditors  : lqm283
  * --------------------------------------------------------------------------------<
  * @Description  : 测试常规的非数组非指针 char 类型
@@ -118,5 +118,6 @@ int test_change_equal_mult_num_json_to_non_non_mult_char_num(char* json);
 int test_change_more_mult_num_json_to_non_non_mult_char_num(char* json);
 int test_change_less_mult_num_json_to_non_non_mult_char_num(char* json);
 int test_change_diff_mult_num_json_to_non_non_mult_char_num(char* json);
+int test_change_equal_mult_bool_json_to_non_non_mult_char_num(char* json);
 
 #endif
