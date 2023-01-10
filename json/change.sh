b33872660635fb713a019cb76cc9b901ce9d3b0c
lqm283 <lanqianming@hotmail.com>
2023-01-10 17:21:59 +0800

    创建了uint8的测试json

--- json/change.sh
+++ json/change.sh
@@ -2,15 +2,15 @@
 ###
  # @Author       : lqm283
  # @Date         : 2023-01-09 08:31:53
- # @LastEditTime : 2023-01-09 22:07:59
+ # @LastEditTime : 2023-01-10 17:10:55
  # @LastEditors  : lqm283
  # --------------------------------------------------------------------------------<
  # @Description  : Please edit a descrition about this file at here.
  # --------------------------------------------------------------------------------<
- # @FilePath     : /jsonc/json/char/ptr_arr/mult/bool/change.sh
+ # @FilePath     : /jsonc/json/uint8/non_arr/mult/bool/change.sh
 ###
 
-for var in `ls *_non_non_*.json`;
+for var in `ls *_char_*.json`;
 do
-    mv "$var" `echo "$var" | awk -F '_non_non_' '{print $1 "_ptr_arr_" $2}'`;
+    mv "$var" `echo "$var" | awk -F '_char_' '{print $1 "_uint8_" $2}'`;
 done
